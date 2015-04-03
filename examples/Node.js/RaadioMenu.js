afd066fdca248f3a864a2dd58e0e2452309d387e
fred-b <frederic.boulet@gmail.com>
2015-04-03 18:24:41 +0200

    start decoupling

--- examples/Node.js/RaadioMenu.js
+++ examples/Node.js/RaadioMenu.js
@@ -1,88 +1,65 @@
+/* global __dirname, exports */
+
 var paper = require('paper');
-paper.setup(new paper.Canvas(128, 64));
-
-var layer = paper.project.activeLayer;
-
 var TWEEN = require('tween.js');
 
 var animDuration = 2000; //ms
 var fps = 20;
-
 var nbFrames = animDuration * fps / 1000;
-
 var keys = [];
-
 for (var i = 0; i < nbFrames; i++) {
     keys.push(i * animDuration / nbFrames);
 }
-
-console.log(keys)
-
 var currentKeyIndex = 0;
 
-initialize();
+var center = paper.view.center;
+var offscreen = paper.view.center.add(128, 0);
 
-function initialize() {
-
-    var center = paper.view.center;
-    var offscreen = paper.view.center.add(128, 0)
-
-    var nova = new paper.Raster({
+var icons = {
+    nova: new paper.Raster({
         source: '/home/fred/rpi/paper.js/examples/raadioMenu/nova.png',
         position: offscreen
-    });
-
-    var inter = new paper.Raster({
+    }),
+    inter: new paper.Raster({
         source: '/home/fred/rpi/paper.js/examples/raadioMenu/inter.png',
         position: offscreen //off-screen but loaded
-    });
+    })
+};
+
+exports.changeRadio = function (from, to, callback) {
+    var from = icons[from];
+    var to = icons[to];
 
     new TWEEN.Tween({x: center.x})
             .to({x: offscreen.x}, animDuration)
             .onUpdate(function () {
-                nova.position.x = this.x
+                from.position.x = this.x;
             })
             .easing(TWEEN.Easing.Exponential.InOut)
-            .start(0)
+            .start(0);
 
     new TWEEN.Tween({x: offscreen.x})
             .to({x: center.x}, animDuration)
             .onUpdate(function () {
-                inter.position.x = this.x
+                to.position.x = this.x;
             })
             .easing(TWEEN.Easing.Exponential.InOut)
-            .start(0)
+            .start(0);
 
-//    new TWEEN.Tween({x: center.x})
-//            .to({x: offscreen.x}, animDuration)
-//            .onUpdate(function () {
-//                inter.position.x = this.x
-//            })
-//            .easing(TWEEN.Easing.Exponential.InOut)
-//            .start(animDuration / 2)
-//
-//    new TWEEN.Tween({x: offscreen.x})
-//            .to({x: center.x}, animDuration)
-//            .onUpdate(function () {
-//                nova.position.x = this.x
-//            })
-//            .easing(TWEEN.Easing.Exponential.InOut)
-//            .start(animDuration / 2)
-}
+    paper.view.onFrame = function (event) {
+        TWEEN.update(keys[currentKeyIndex]);
+    };
 
-paper.view.onFrame = function (event) {
-    console.log(keys[currentKeyIndex])
-    TWEEN.update(keys[currentKeyIndex]);
+    paper.view.exportFrames({
+        amount: nbFrames,
+        directory: __dirname,
+        onComplete: function () {
+            console.log('Done exporting.');
+        },
+        onProgress: function (event) {
+            currentKeyIndex++;
+            console.log(event.percentage + '% complete, frame took: ' + event.delta);
+            callback();
+        }
+    });
 };
-
-paper.view.exportFrames({
-    amount: nbFrames,
-    directory: __dirname,
-    onComplete: function () {
-        console.log('Done exporting.');
-    },
-    onProgress: function (event) {
-        currentKeyIndex++;
-        console.log(event.percentage + '% complete, frame took: ' + event.delta);
-    }
-});
