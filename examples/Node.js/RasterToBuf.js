86bd6a7f4280b143f0222b51fc1ac0d30dede0b7
fred-b <frederic.boulet@gmail.com>
2015-05-24 18:35:55 +0200

    workaround frame pétée

--- examples/Node.js/RasterToBuf.js
+++ examples/Node.js/RasterToBuf.js
@@ -11,37 +11,41 @@ renderRasters = function (callback) {
     var animDuration = 3000; //ms
     var fps = 21;
     var nbFrames = animDuration * fps / 1000;
-    var keys = [];
-    for (var i = 1; i <= nbFrames; i++) {
-        keys.push(i * animDuration / nbFrames);
-    }
-    var currentKeyIndex = 0;
-    
+
+    /**
+     * bizarrement la frame-00 est blanche, on décale tout pour garder les 
+     * mm indices que sur la sequence de départ, et on remet la frame 0 à l'index 
+     * nbframe+1
+     * @type Number
+     */
+
+    var currentFrameIndex = 1;
+
     paper.view.onFrame = function (event) {
-        paper.project.activeLayer.removeChildren();
-        
-        var index = ("00" + currentKeyIndex).slice(-2);
+        console.log("onframe " + currentFrameIndex)
+
+        var index = ("00" + currentFrameIndex % nbFrames).slice(-2);
         new paper.Raster({
             source: '/home/fred/Images/frames/' + index + '.png',
             position: center
-        })
+        });
     };
 
     paper.view.exportFrames({
-        amount: nbFrames,
+        amount: nbFrames + 1,
         directory: __dirname,
         onComplete: function () {
             console.log('Done exporting.');
             callback();
         },
         onProgress: function (event) {
-            currentKeyIndex++;
+            currentFrameIndex++;
             console.log(event.percentage + '% complete, frame took: ' + event.delta);
         }
     });
 };
 
 
-renderRasters(function(){
+renderRasters(function () {
     console.log("fini")
 })
